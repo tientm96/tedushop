595263f6151fda65fb44e4ca54adc2de75725251
Tran Minh Tien <32189164+tientm96@users.noreply.github.com>
2018-05-17 23:38:54 +0700

    add entity frameword for project data - update

---
+++ TeduShop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TeduShop.Data.Infrastructure
+{
+    //IDisposable là Interface có sẵn: cho phép các class kế thừa từ nó sẽ tự động hủy.
+    public interface IDbFactory : IDisposable
+    {
+        TeduShopDbContext Init();
+    }
+}
