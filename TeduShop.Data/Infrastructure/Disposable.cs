ff9b79b651639921848af7b60037c407d064bf8f
Tran Minh Tien <32189164+tientm96@users.noreply.github.com>
2018-05-17 23:12:53 +0700

    add entity framework code first for project data

---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    //IDisposable là Interface có sẵn: cho phép các class kế thừa từ nó sẽ tự động hủy.
+    public class Disposable : IDisposable
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            isDisposed = true;
+        }
+
+        // Ovveride this to dispose custom objects
+        protected virtual void DisposeCore()
+        {
+        }
+    }
+}
