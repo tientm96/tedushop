4461a74540bc565673e6285ba966c5dbe5c91c0c
Tran Minh Tien <32189164+tientm96@users.noreply.github.com>
2018-05-21 23:37:18 +0700

    fixbug for repositories and add post service

--- TeduShop.Model/Models/Post.cs
+++ TeduShop.Model/Models/Post.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using TeduShop.Model.Abstract;
 
@@ -35,8 +37,11 @@ namespace TeduShop.Model.Models
         public bool? HotFlag { set; get; }
         public int? ViewCount { set; get; }
 
-        //Tạo khóa ngoại
+
+        //tạo khóa ngoại
         [ForeignKey("CategoryID")]
         public virtual PostCategory PostCategory { set; get; }
+
+        public virtual IEnumerable<PostTag> PostTags { set; get; }
     }
 }
