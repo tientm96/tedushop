400e8c57a246af9c475b8b332f213736fd20c2fc
DESKTOP-FQII8RQ\Tien's PC <tien.tran@newoceaninfosys.com>
2018-10-19 18:42:12 +0700

    add comment

--- TeduShop.Model/Models/Error.cs
+++ TeduShop.Model/Models/Error.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TeduShop.Model.Models
 {
+    //table này: chứa các lỗi được log ra.
     [Table("Errors")]
     public class Error
     {
